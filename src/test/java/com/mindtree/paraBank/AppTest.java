936f4c33f335bb45e93210350f243357249745dd
Testlabs-Acoe <80874679+Testlabs-Acoe@users.noreply.github.com>
2022-12-21 06:29:47 +0000

    Update AppTest.java

--- src/test/java/com/mindtree/paraBank/AppTest.java
+++ src/test/java/com/mindtree/paraBank/AppTest.java
@@ -3,8 +3,8 @@ package com.mindtree.paraBank;
 import java.util.Date;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
@@ -17,60 +17,65 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AppTest {
 	
-	public static RemoteWebDriver driver;
+	public static WebDriver driver;
 	public static ExtentReports extent;
 	public static ExtentTest test;
 	
 	@BeforeSuite
 	public void beforeSuite() throws Exception {
-		System.out.println("print from before suite");
-		Date curentDate =new Date();
-		String reportFilename = curentDate.toString().replace(" ", "-").replace(":", "-");
-		extent = new ExtentReports(".//Reports//"+reportFilename+".html",true);
+		System.out.println("print from before suit");
+		Date currentDate = new Date();
+		String reportFilename = currentDate.toString().replace(" ", "-").replace(":", "-");
+		extent = new ExtentReports(".//Reports//"+reportFilename+".html", true);
 	}
-	
+
 	@BeforeMethod
 	public void beforeMethod() {
 		System.out.println("print from before method");
 		test = extent.startTest("paraBank");
+		test.log(LogStatus.PASS, "Driver started successFully");
 	}
 	
 	@BeforeTest
 	public void beforeTest() {
-		WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\azureuser\\Desktop\\SmartExecution_VM_local_project\\VMDemo\\Driver\\chromedriver.exe");
 		driver = new ChromeDriver();
 	}
-	
-	@Test
+
+	@Test()
 	public void test_1() throws Exception {
 		System.out.println("print from test");
-		driver.get("https://parabank.parasoft.com/index.html");
-		driver.manage().window().maximize();
+		driver.get("http://localhost:9090/para");
 		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("john");
 		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("demo");
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
 		test.log(LogStatus.PASS, "test passed");
-	}
+ 
+		
+	}	
 	
 	@AfterMethod
 	public void afterMethod() {
 		extent.endTest(test);
 	}
-	
+
 	@AfterTest
 	public void afterTest() {
 		driver.close();
 		driver.quit();
+		test.log(LogStatus.PASS, "Driver closed Successfully");
 	}
-	
+
 	@AfterSuite
 	public void afterSuite() {
 		extent.flush();
 		extent.close();
 	}
+
+
+}
 
 }
