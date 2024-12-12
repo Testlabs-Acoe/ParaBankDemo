fe7fd5d0e5c7d99aefb44605aa6125590784bc93
Testlabs-Acoe <80874679+Testlabs-Acoe@users.noreply.github.com>
2024-12-12 14:08:00 +0530

    Update AppTest.java
    Updating test script

--- src/test/java/com/mindtree/paraBank/AppTest.java
+++ src/test/java/com/mindtree/paraBank/AppTest.java
@@ -13,71 +13,62 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
-
 public class AppTest {
-	
-	public static WebDriver driver;
-	public static ExtentReports extent;
-	public static ExtentTest test;
-	
-	@BeforeSuite
-	public void beforeSuite() throws Exception {
-		System.out.println("print from before suit");
-		Date currentDate = new Date();
-		String reportFilename = currentDate.toString().replace(" ", "-").replace(":", "-");
-		extent = new ExtentReports(".//Reports//"+reportFilename+".html", true);
-	}
+    
+    public static WebDriver driver;
+    public static ExtentReports extent;
+    public static ExtentTest test;
+    
+    @BeforeSuite
+    public void beforeSuite() throws Exception {
+        System.out.println("print from before suite");
+        Date currentDate = new Date();
+        String reportFilename = currentDate.toString().replace(" ", "-").replace(":", "-");
+        extent = new ExtentReports("./Reports/" + reportFilename + ".html", true);
+    }
 
-	@BeforeMethod
-	public void beforeMethod() {
-		System.out.println("print from before method");
-		test = extent.startTest("paraBank");
-		test.log(LogStatus.PASS, "Driver started successFully");
-	}
-	
-	@BeforeTest
-	public void beforeTest() {
-		// System.setProperty("webdriver.chrome.driver", "C:\\Users\\azureuser\\Desktop\\SmartExecution_VM_local_project\\VMDemo\\Driver\\chromedriver.exe");
-		WebDriverManager.chromedriver().driverVersion("131.0.6778.86").setup();
-		driver = new ChromeDriver();
-	}
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("print from before method");
+        test = extent.startTest("paraBank");
+        test.log(LogStatus.PASS, "Driver started successfully");
+    }
+    
+    @BeforeTest
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver"); // Update the path to the ChromeDriver executable
+        driver = new ChromeDriver();
+    }
 
-	@Test()
-	public void test_1() throws Exception {
-		System.out.println("print from test");
-		// driver.get("http://localhost:9090/para");
-		driver.get("https://parabank.parasoft.com/parabank/index.htm");
-		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("john");
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("demo");
-		driver.findElement(By.xpath("//input[@type='submit']")).click();
-		test.log(LogStatus.PASS, "test passed");
- 
-		
-	}	
-	
-	@AfterMethod
-	public void afterMethod() {
-		extent.endTest(test);
-	}
+    @Test
+    public void test_1() throws Exception {
+        System.out.println("print from test");
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("john");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("demo");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+        test.log(LogStatus.PASS, "test passed");
+    }    
+    
+    @AfterMethod
+    public void afterMethod() {
+        extent.endTest(test);
+    }
 
-	@AfterTest
-	public void afterTest() {
-		driver.close();
-		driver.quit();
-		test.log(LogStatus.PASS, "Driver closed Successfully");
-	}
+    @AfterTest
+    public void afterTest() {
+        driver.close();
+        driver.quit();
+        test.log(LogStatus.PASS, "Driver closed successfully");
+    }
 
-	@AfterSuite
-	public void afterSuite() {
-		extent.flush();
-		extent.close();
-	}
-
-
+    @AfterSuite
+    public void afterSuite() {
+        extent.flush();
+        extent.close();
+    }
 }
